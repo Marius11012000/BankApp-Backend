64366b4419c939f43dd4822f658328847f9e2323
Marius <marius.florescu@fancourier.ro>
2024-03-20 23:42:26 +0200

    Integrare cu cosmos db

--- config.py
+++ config.py
@@ -2,4 +2,5 @@
 
 class Config:
     SECRET_KEY = 'oasecretfoartesecreta'
-    MONGO_URI = 'mongodb://localhost:27017/Bank'
+    #MONGO_URI = 'mongodb://localhost:27017/Bank'
+    MONGO_URI = 'mongodb+srv://<user>:<password>@bankappetti.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000'
