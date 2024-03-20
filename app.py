350cc0cd337331196ec6721a5a52bf212344e78b
Marius <marius.florescu@fancourier.ro>
2024-03-21 00:30:21 +0200

    Modificare app.py

--- app.py
+++ app.py
@@ -1,47 +1,14 @@
-# from flask import Flask, request, jsonify
-# from flask_pymongo import PyMongo
-# from werkzeug.security import generate_password_hash, check_password_hash
-#
-# app = Flask(__name__)
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/mydatabase"
-# mongo = PyMongo(app)
-#
-# @app.route('/register', methods=['POST'])
-# def register():
-#     data = request.get_json()
-#     username = data.get('username')
-#     email = data.get('email')
-#     password = data.get('password')
-#     cnp = data.get('cnp')
-#     phone = data.get('phone')
-#     # Validează datele...
-#     if username and email and password and cnp and phone:
-#         hashed_password = generate_password_hash(password)
-#         mongo.db.users.insert_one({
-#             'username': username,
-#             'email': email,
-#             'password': hashed_password,
-#             'cnp': cnp,
-#             'phone': phone
-#         })
-#         return jsonify({'message': 'Registered successfully'}), 201
-#     else:
-#         return jsonify({'message': 'Missing data'}), 400
-#
-# @app.route('/login', methods=['POST'])
-# def login():
-#     data = request.get_json()
-#     user = mongo.db.users.find_one({'email': data.get('email')})
-#     if user and check_password_hash(user['password'], data.get('password')):
-#         return jsonify({'message': 'Login successful'}), 200
-#     else:
-#         return jsonify({'message': 'Invalid username or password'}), 401
+#from app import create_app
 
+#app = create_app()
 
-from app import create_app
+#if __name__ == '__main__':
+#    app.run(host='0.0.0.0', port=5000)
 
-app = create_app()
+from flask import Flask
+from pymongo import MongoClient
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
+app = Flask(__name__)
+mongo_uri = "mongodb+srv://marius:Mongodb2000.@bankappetti.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000"
+client = MongoClient(mongo_uri)
+db = client.yourdbname
