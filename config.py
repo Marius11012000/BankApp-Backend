bef2a9db94b6deac814bc87c4127e6fa6d10d0cc
Marius <marius.florescu@fancourier.ro>
2024-03-21 00:23:13 +0200

    Modificare MONGO_URI

--- config.py
+++ config.py
@@ -3,4 +3,4 @@
 class Config:
     SECRET_KEY = 'oasecretfoartesecreta'
     #MONGO_URI = 'mongodb://localhost:27017/Bank'
-    MONGO_URI = 'mongodb+srv://<user>:<password>@bankappetti.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000'
+    MONGO_URI = 'mongodb+srv://marius:Mongodb2000.@bankappetti.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000'
